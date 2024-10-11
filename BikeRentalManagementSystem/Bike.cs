0b7291da026e6209c0d4f3d6d35ed4c3793df51e
UT02009 <UT02009@UNICOMTIC20>
2024-10-11 11:04:39 +0530

    Add project files.

---
+++ BikeRentalManagementSystem/Bike.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRentalManagementSystem
+{
+    public class Bike
+    {
+        public string BikeId { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public decimal RentalPrice { get; set; }
+
+        public Bike(string bikeId, string brand, string model, decimal rentalPrice)
+        {
+            BikeId = bikeId;
+            Brand = brand;
+            Model = model;
+            RentalPrice = rentalPrice;
+        }private decimal RentalAmount { get;set; }
+
+        public override string ToString()
+        {
+            return $"bikeId : {BikeId}, brand: {Brand}, model: {Model}, rentalPrice: {RentalPrice}";
+        }
+    }
+    
+}
---
+++ BikeRentalManagementSystem/BikeManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRentalManagementSystem
+{
+    internal class BikeManager
+    {
+        private List<Bike> bikes = new 
+    }
+}
